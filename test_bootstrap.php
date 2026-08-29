f982a2634b1d5f929eea273a7c9d74f8364d40d5
tonic phiri <phiritonic550@gmail.com>
2026-08-29 15:01:51 +0200

    start next round

---
+++ test_bootstrap.php
@@ -0,0 +1,4 @@
+<?php
+require 'vendor/autoload.php';
+$app = require __DIR__.'/bootstrap/app.php';
+echo 'bootstrap OK';
---
+++ test_phpinfo.php
@@ -0,0 +1,4 @@
+<?php
+echo "PHP_SAPI: " . PHP_SAPI . "\n";
+echo "PHP Binary: " . PHP_BINARY . "\n";
+echo "Doc Root: " . ini_get('doc_root') . "\n";
